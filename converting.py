a091829cfb028b3e5491f4434d11a0e3dab811fe
Theodor René Carlsen <theodor.rene@gmail.com>
2018-12-13 12:41:05 +0100

    Add comments

--- converting.py
+++ converting.py
@@ -6,20 +6,27 @@ import sqlite3
 from sys import argv
 import subprocess
 
+#Global variable for the solution of todays puzzle
 ANSWER = ""
 
 
+#This command downloads the image from then fen into the picture "position.png"
 def converting(fen):
     cmd = 'curl http://www.fen-to-image.com/image/36/single/coords/' + fen + ' | cat > position.png'
     subprocess.Popen(cmd,shell=True)
 
 def main():
+    #Get fen and color to move
     fen,player = query()
+    #get image of position
     converting(fen)
+    #wait until image is retrieved
     sleep(2)
+    #Tweet 
     tweet(player)
 
 def tweet(player):
+    #Get keys from text file
     c_k,c_s,a_k,a_s = getKeys()
     auth = tweepy.OAuthHandler(c_k, c_s)
     auth.set_access_token(a_k, a_s)
@@ -35,11 +42,13 @@ def tweet(player):
 
     api.update_with_media(image,status=message)
 
+    #Wait until last tweet has gone through before postin reply
     sleep(5)
 
     last_tweet = api.user_timeline(id=myID, count = 1)[0]
     api.update_status("@ChessDaily Solution:"+ ANSWER, in_reply_to_status_id=last_tweet.id)
 
+#get keys and parsing
 def getKeys():
     f = open('keys.txt','r')
     l = []
@@ -52,6 +61,7 @@ def query():
     c = conn.cursor()
     global ANSWER
 
+    #Get the count from database, this has to update so new puzzle are fetched
     getCount = "SELECT number FROM count"
     count = c.execute(getCount).fetchone()
     newCount = count[0] + 1
@@ -65,6 +75,7 @@ def query():
     c.execute(updateCount)
     fen = c.execute(getFen).fetchone()
     player = c.execute(getPlayer).fetchone()
+
     conn.commit()
     conn.close()
     return fen[0],player[0]
--- makeDB.py
+++ makeDB.py
@@ -13,13 +13,16 @@ def reFormating(line):
     l = line.split(" ")
     return l[0],l[1]
 
+#Parses the txtfile and adds entries to the database
 def makeList(f):
     conn = sqlite3.connect('mateIn4.db')
     c = conn.cursor()
+    #Make table
     c.execute("CREATE TABLE puzzle (id INTEGER PRIMARY KEY,fen text, to_move text,solution text)")
     l = []
     lines = f.read().splitlines()
     for i in range(2,len(lines)-1):
+        #the fen comes at lines where i-1%5 is equal to zero
         if (i-1)%5 == 0:
             solution = lines[i+1]
             fen,color = reFormating(lines[i])
@@ -27,6 +30,7 @@ def makeList(f):
             l.append(tup)
             dbReq = f"INSERT INTO puzzle (fen,to_move,solution) VALUES ('{fen}','{color}','{solution}')"
             c.execute(dbReq)
+
     conn.commit()
     conn.close()
     return l
