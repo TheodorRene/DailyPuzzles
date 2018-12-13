f2db27cf2724f36690376b62e775b78b50536b0b
Theodor René Carlsen <theodor.rene@gmail.com>
2018-12-13 21:11:25 +0100

    Reformating

--- makeDB.py
+++ makeDB.py
@@ -3,27 +3,27 @@
 import sqlite3
 from sys import argv
 
+# \s*([rnbqkpRNBQKP1-8]+\/){7}([rnbqkpRNBQKP1-8]+)\s[bw-]\s(([a-hkqA-HKQ]{1,4})|(-))\s(([a-h][36])|(-))\s\d+\s\d+\s* possible regex for verifing FEN
+
 
 def main():
     if argv[1]=="-a":
         add(argv[2])
     else:
         f = open('MateIn4.txt','r')
-        l = makeList(f)
+        makeList(f)
         f.close()
 
+#Add more puzzles to database. Remeber formatting from matein4.txt
 def add(file)
     f = open(file,'r')
     conn = sqlite3.connect('mateIn4.db')
     c = conn.cursor()
-    l = []
     lines = f.read().splitlines()
     for i in range(2,len(lines)-1):
         if (i-1)%5 == 0:
             solution = lines[i+1]
             fen,color = reFormating(lines[i])
-            tup = (fen,color,solution)
-            l.append(tup)
             dbReq = f"INSERT INTO puzzle (fen,to_move,solution) VALUES ('{fen}','{color}','{solution}')"
             c.execute(dbReq)
     conn.commit()
@@ -39,20 +39,16 @@ def makeList(f):
     c = conn.cursor()
     #Make table
     c.execute("CREATE TABLE puzzle (id INTEGER PRIMARY KEY,fen text, to_move text,solution text)")
-    l = []
     lines = f.read().splitlines()
     for i in range(2,len(lines)-1):
         #the fen comes at lines where i-1%5 is equal to zero
         if (i-1)%5 == 0:
             solution = lines[i+1]
             fen,color = reFormating(lines[i])
-            tup = (fen,color,solution)
-            l.append(tup)
             dbReq = f"INSERT INTO puzzle (fen,to_move,solution) VALUES ('{fen}','{color}','{solution}')"
             c.execute(dbReq)
 
     conn.commit()
     conn.close()
-    return l
 
 main()
