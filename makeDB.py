0a846e1aa80b9987819453b65439d600e29fda1b
theodorc <theodor.rene@gmail.com>
2019-07-24 21:42:20 +0200

    Add creation of count table and fix conditional

--- makeDB.py
+++ makeDB.py
@@ -2,23 +2,26 @@
 
 import sqlite3
 from sys import argv
+from os import path
 
 # \s*([rnbqkpRNBQKP1-8]+\/){7}([rnbqkpRNBQKP1-8]+)\s[bw-]\s(([a-hkqA-HKQ]{1,4})|(-))\s(([a-h][36])|(-))\s\d+\s\d+\s* possible regex for verifing FEN
 
 
 def main():
-    if argv[1]=="-a":
-        print("Adding " + argv[2] + " to the puzzle database") 
+    if len(argv)>1 and argv[1]=="-a":
+        print("Adding " + argv[2] + " to the puzzle database")
         add(argv[2])
         print("Database has been updated")
     else:
+        if(path.exists("mateIn4.db")):
+            raise Exception('Database file already exists')
         print("Making database from MateIn4.txt")
         f = open('MateIn4.txt','r')
         makeList(f)
         f.close()
-        print("MateIn4.db has been made)
+        print("MateIn4.db has been made")
 
-#Add more puzzles to database. Remeber formatting from matein4.txt
+#Add more puzzles to database. Remember formatting from matein4.txt
 def add(file):
     f = open(file,'r')
     conn = sqlite3.connect('mateIn4.db')
@@ -51,6 +54,9 @@ def makeList(f):
             fen,color = reFormating(lines[i])
             dbReq = f"INSERT INTO puzzle (fen,to_move,solution) VALUES ('{fen}','{color}','{solution}')"
             c.execute(dbReq)
+    # make counting table
+    c.execute("CREATE TABLE count (id INTEGER PRIMARY KEY, number INTEGER)")
+    c.execute("INSERT INTO count (number) VALUES (1)")
 
     conn.commit()
     conn.close()
